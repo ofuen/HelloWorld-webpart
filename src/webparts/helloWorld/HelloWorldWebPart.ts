0f0f91f2c3821ef3ac11b95c069566770970e38e
oscar <oscar@o2webdev.com>
2019-01-29 16:52:49 -0500

    :zap:

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,4 +1,8 @@
-import { Version } from '@microsoft/sp-core-library';
+import { 
+  Version,
+  Environment,
+  EnvironmentType
+   } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
@@ -11,6 +15,13 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './HelloWorldWebPart.module.scss';
 import * as strings from 'HelloWorldWebPartStrings';
+import MockHttpClient from './MockHttpClient';
+
+import {
+  SPHttpClient,
+  SPHttpClientResponse
+} from '@microsoft/sp-http';
+
 
 export interface IHelloWorldWebPartProps {
   description: string;
@@ -18,6 +29,15 @@ export interface IHelloWorldWebPartProps {
   test1: boolean;
   test2: string;
   test3: boolean;
+}
+
+export interface ISPLists {
+  value: ISPList[];
+}
+
+export interface ISPList {
+  Title: string;
+  Id: string;
 }
 
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
@@ -38,8 +58,56 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
              </a>
            </div>
          </div>
+         <div id="spListContainer" />
        </div>
      </div>`;
+
+    this._renderListAsync();
+  }
+
+  private _getMockListData(): Promise<ISPLists> {
+    return MockHttpClient.get()
+    .then((data: ISPList[]) => {
+      var listData: ISPLists = { value: data };
+      return listData;
+    }) as Promise<ISPLists>;
+  }
+
+  private _getListData(): Promise<ISPLists> {
+    return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=Hidden eq false`, SPHttpClient.configurations.v1)
+    .then((response: SPHttpClientResponse) => {
+      return response.json();
+    });
+  }
+
+  private _renderList(items: ISPList[]): void {
+    let html: string = '';
+    items.forEach((item: ISPList) => {
+      html += `
+      <ul class="${styles.list}">
+      <li class="${styles.listItem}">
+      <span class="ms-font-l">${item.Title}</span>
+      </li>
+      </ul>`;
+    });
+
+    const listContainer: Element = this.domElement.querySelector('#spListContainer');
+    listContainer.innerHTML = html;
+  }
+
+  private _renderListAsync(): void {
+    // Local environment
+    if (Environment.type === EnvironmentType.Local) {
+      this._getMockListData().then((response) => {
+        this._renderList(response.value);
+      });
+    }
+    else if (Environment.type == EnvironmentType.SharePoint || 
+              Environment.type == EnvironmentType.ClassicSharePoint)
+              this._getListData()
+              .then((response) => {
+                this._renderList(response.value);
+              });
   }
 
   protected get dataVersion(): Version {
